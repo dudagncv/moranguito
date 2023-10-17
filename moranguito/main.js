a0dfed014c248dd19126dd8336cae5dee4a7aaf8
dudagncv <125581993+dudagncv@users.noreply.github.com>
2023-10-17 07:54:01 -0300

    Update main.js

--- moranguito/main.js
+++ moranguito/main.js
@@ -1,3 +1,6 @@
-alert ("Olá mundo!")
-document.querySelector('.tecla_pom')
+function tocasSomPom () {
 document.querySelector('#som_tecla_pom').play();
+}
+const ListasDeTecla = document.querySelectorAll('.tecla');
+
+ListaDeTeclas{0}.onclick = tocaSomPom;
