1cf146bc4feddae4977e6c761860f806b7893128
dudagncv <125581993+dudagncv@users.noreply.github.com>
2023-11-16 07:47:08 -0300

    Update main.js

--- moranguito/main.js
+++ moranguito/main.js
@@ -1,6 +1,19 @@
 function tocasSomPom () {
 document.querySelector('#som_tecla_pom').play();
 }
-const ListasDeTecla = document.querySelectorAll('.tecla');
 
-ListaDeTeclas{0}.onclick = tocaSomPom;
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador= 0;
+
+//enquanto
+while (contador < listaDeTeclas.length ){
+
+listaDeTeclas{0}.onclick = tocaSomPom;
+
+contador= contador + 1;
+
+console.log(contador);
+
+}
+
